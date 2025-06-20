7a480548c58fb20a217c32236d5a9d6c8705ddcc
renit <35866420+renit0424@users.noreply.github.com>
2025-06-20 14:03:52 +0900

    Update weather.js

--- test/js_css/weather.js
+++ test/js_css/weather.js
@@ -2,7 +2,7 @@ timerID = setInterval('weather()', 10000);
 weather();
 
 function weather() {
-	var url = 'weather.php';
+	var url = 'https://renitapps.com/test/weather.php';
 	fetch(url).then(function(response) {
 		return response.json();
 	}).then(function(json) {
