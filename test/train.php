9d6574014fc41516f502bc793e343fc466fa8c86
renit <35866420+renit0424@users.noreply.github.com>
2025-06-20 13:50:00 +0900

    Update train.php

--- test/train.php
+++ test/train.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-$url = "https://www.train-guide.westjr.co.jp/api/v3/sanin4.json";
+#$url = "https://www.train-guide.westjr.co.jp/api/v3/sanin4.json"; //山陰本線
+$url = "https://www.train-guide.westjr.co.jp/api/v3/tozai.json"; //JR東西線
 $json = file_get_contents($url);
 $json = mb_convert_encoding($json, 'utf-8');
 $arr = json_decode($json,true);
